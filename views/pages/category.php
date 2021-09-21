6d310eb19999fa42bed06f57a248118d147fbd99
Ricardo García Jiménez <ricardogj08@riseup.net>
2021-09-20 22:50:35 -0500

    Añade maquetado simple de la páginas de categoría y confirmar contraseña

---
+++ views/pages/category.php
@@ -0,0 +1,11 @@
+<?php defined('NABU') || exit ?>
+<?php $head_title = 'Categorías' ?>
+<?php $styles     = array(
+    NABU_DIRECTORY['styles'] . '/pages/category/category.css',
+    NABU_DIRECTORY['styles'] . '/pages/category/category-desktop.css',
+) ?>
+<?php require_once 'views/components/head.php' ?>
+
+<h1>Categorías</h1>
+
+<?php require_once 'views/components/footer.php' ?>
---
+++ views/pages/confirm-password.php
@@ -0,0 +1,32 @@
+<?php defined('NABU') || exit ?>
+<?php $head_title = 'Confirmar contraseña' ?>
+<?php $styles     = array(
+    NABU_DIRECTORY['styles'] . '/pages/confirm-password/confirm-password.css',
+    NABU_DIRECTORY['styles'] . '/pages/confirm-password/confirm-password-desktop.css',
+) ?>
+<?php require_once 'views/components/head.php' ?>
+
+<h1>Confirmar contraseña</h1>
+
+<p><a href="<?= $back ?>">Volver</a></p>
+
+<?php require_once 'views/components/messages.php' ?>
+
+<p>Ingresa tu contraseña para completar la operación.</p>
+
+<form method="POST" action="<?= $view ?>">
+    <input type="hidden" name="csrf" value="<?= $token ?>">
+    <div>
+        <label for="password">Contraseña</label>
+        <input type="password" id="password" name="password" minleght="6" maxlenght="255" required>
+    </div>
+    <div>
+        <label for="password-confirm">Confirmar contraseña</label>
+        <input type="password" id="password-confirm" name="password-confirm" minlenght="6" maxlenght="255" required>
+    </div>
+    <div>
+        <input type="submit" name="confirm-password-submit" value="Enviar">
+    </div>
+</form>
+
+<?php require_once 'views/components/footer.php' ?>
