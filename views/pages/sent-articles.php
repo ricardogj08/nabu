a768ff04f2c857c5b8cde8dc8b437dfcfa3b49c5
Ricardo García Jiménez <ricardogj08@riseup.net>
2021-09-22 20:21:08 -0500

    [Agrega] maquetado simple para la página de registro y edición de artículos para los usuarios

---
+++ views/pages/sent-articles.php
@@ -0,0 +1,31 @@
+<?php defined('NABU') || exit ?>
+<?php $head_title = 'Artículos enviados' ?>
+<?php $styles     = array(
+    NABU_DIRECTORY['styles'] . '/pages/sent-articles/sent-articles.css',
+    NABU_DIRECTORY['styles'] . '/pages/sent-articles/sent-articles-desktop.css',
+) ?>
+<?php require_once 'views/components/head.php' ?>
+<?php require_once 'views/components/navbar.php' ?>
+
+<h1>Artículos en espera de aprobación</h1>
+
+<?php require_once 'views/components/messages.php' ?>
+
+<?php if (!empty($articles)): ?>
+<table>
+    <tr>
+        <td>Título</td>
+        <td></td>
+        <td></td>
+1    </tr>
+    <?php foreach ($articles as $article): ?>
+        <tr>
+            <td><?= $article['title'] ?></td>
+            <td><a href="<?= $article['slug'] ?>">Editar</a></td>
+            <td><a href="<?= $article['slug'] ?>">Reenviar</a></td>
+        </tr>
+    <?php endforeach ?>
+</table>
+<?php endif ?>
+
+<?php require_once 'views/components/footer.php' ?>
---
+++ views/pages/signup.php
@@ -0,0 +1,41 @@
+<?php defined('NABU') || exit ?>
+<?php $head_title = 'Crea una cuenta' ?>
+<?php $styles     = array(
+    NABU_DIRECTORY['styles'] . '/pages/signup/signup.css',
+    NABU_DIRECTORY['styles'] . '/pages/signup/signup-desktop.css',
+) ?>
+<?php require_once 'views/components/head.php' ?>
+
+<h1>Crea una cuenta</h1>
+
+<?php require_once 'views/components/messages.php' ?>
+
+<form method="POST" action="<?= NABU_ROUTES['signup'] ?>">
+    <input type="hidden" name="csrf" value="<?= $token ?>">
+    <div>
+        <label for="name">Nombre completo</label>
+        <input type="text" id="name" name="name" minlength="5" maxlength="255" autofocus required>
+    </div>
+    <div>
+        <label for="username">Apodo</label>
+        <input type="text" id="username" name="username" minlength="1" maxlength="255" required>
+    </div>
+    <div>
+        <label for="email">Correo institucional</label>
+        <input type="email" id="email" name="email" minlength="5" maxlength="255" required>
+    </div>
+    <div>
+        <label for="password">Constraseña</label>
+        <input type="password" id="password" name="password" minlength="6" maxlength="255" required>
+    </div>
+    <div>
+        <label for="confirm-password">Confirmar contraseña</label>
+        <input type="password" id="confirm-password" name="confirm-password" minlength="6" maxlength="255" required>
+    </div>
+    <div>
+        <input type="submit" name="signup-submit" value="Registrarme">
+        <p>¿Ya tienes cuenta? <a href="<?= NABU_ROUTES['login'] ?>">Inicia sesión</a>.</p>
+    </div>
+</form>
+
+<?php require_once 'views/components/footer.php' ?>
