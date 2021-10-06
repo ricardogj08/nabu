9c6566196b9582df84ea0fd6e050436e9049a576
Ricardo García Jiménez <ricardogj08@riseup.net>
2021-10-06 16:39:52 -0500

    [Valida] la autentificación del hash y su expiración en las verificaciones

--- controllers/verificationsController.php
+++ controllers/verificationsController.php
@@ -18,5 +18,27 @@ class verificationsController {
         if (empty($user['hash']) || empty($user['hash_expiration'])) {
             utils::redirect(NABU_ROUTES['home']);
         }
+
+        // Reconstruye el hash con la llave de verificación de la URL.
+        $hash = hash_hmac('sha256', $user['email'], $_GET['key']);
+
+        // Valida la autentificación del hash.
+        if (!hash_equals($user['hash'], $hash)) {
+            utils::redirect(NABU_ROUTES['home']);
+        }
+
+        // Valida si el hash está expirado y elimina el usuario.
+        if (time() > $user['hash_expiration']) {
+            require_once 'models/usersModel.php';
+
+            $usersModel = new usersModel();
+            $usersModel -> delete($user['id']);
+
+            messages::add('Tu cuenta ha expirado, por favor vuelva a registrarse');
+            utils::redirect(NABU_ROUTES['signup']);
+        }
+
+        // Activa la cuenta del usuario y crea su perfil.
+        echo "ok";
     }
 }
