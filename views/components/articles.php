07507d6fbac51a287c471050310ebd850bcb8425
francisco-solis99 <franciscosoliscss@gmail.com>
2021-10-15 18:53:08 -0500

    [Arreglar] detalles de dimension de la card y agregando anclas

--- views/components/articles.php
+++ views/components/articles.php
@@ -6,14 +6,18 @@
     </picture>
     <div class="card__info">
         <div class="card__text">
-            <h3 class="card__title">Lugares donde la inspiracion y las ideas pueden llegar</h3>
+             <h3 class="card__title">
+                <a href="#" class="card__title-link">
+                    Lugares donde la inspiracion y las ideas pueden llegar
+                </a>
+             </h3>  
             <p class="card__copy">La creatividad a veces no llega en el momento deseado, pero puede alcanzarnos en el momento menos esperado...</p>
         </div>
         <div class="card__details">
             <picture class="profile">
                 <img src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.6435-9/103564332_100818335008500_4987535241959814468_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=174925&_nc_eui2=AeFLfQ6m44wh0nGXbCIoQp9rQi_G7p1L5IpCL8bunUvkipd7MyfqDBh6lvl5slEJxg2JfKqtfoJLLSFgbZBhQ1tV&_nc_ohc=2Ht3awmMIc0AX975ljD&_nc_ht=scontent-lax3-1.xx&oh=aa5254a401801cdf23a8e02ae0239237&oe=616EB288" alt="" class="profile__image">
             </picture>
-            <span class = "card__author">by Ricardo Adrian Gomez</span>
+            <a class="card__author" href="#">by Ricardo Adrian Gomez</a>
             <div class="card__stats">
                 <span class = "card__stats-hearth"></span>
                 <span class = "card__stats-numlike">12</span>
